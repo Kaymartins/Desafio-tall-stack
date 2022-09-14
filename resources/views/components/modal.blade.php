b2f779a68806eeb602e11526dfc62a279da55100
Kaymartins <kayanmartins1@gmail.com>
2022-09-14 20:40:50 -0300

    componentização modal

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,25 @@
+<div x-show="open">
+    <div :class="{'block':open, 'hidden': ! open}" class="hidden fixed inset-0 bg-white bg-opacity-75 flex items-center justify-center">
+        <div class="flex flex-col bg-white shadow-2xl rounded-lg border-gray-400 p6">
+
+            <div class="px-6 py-2">
+                <h3 class="text-lg font-medium text-rose-800">
+                    {{$header ?? null}}
+                </h3>
+                <form class="space-y-8 divide-y divide-gray-200">
+                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                        {{$content ?? null}}
+
+                        <div class="col-span-2">
+
+                            {{$option ?? null }}
+
+                            <button type="button" x-on:click="open = false" wire:click="clearForm" class="bg-red-500 py-2 px-3 rounded-md shadow-sm text-sm hover:bg-red-800 font-medium text-white cursor-pointer">Fechar</button>
+
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/player/player-index.blade.php
+++ resources/views/livewire/player/player-index.blade.php
@@ -3,76 +3,62 @@
         <div class="col-span-3" x-data="{ open:false }">
             <button type="button" wire:click="clearForm" class="bg-blue-500 py-2 px-3 rounded-md shadow-sm text-sm hover:bg-purple-500 font-medium text-white cursor-pointer" x-on:click="open = ! open">Criar</button>
 
-            <!--MODAL CREATE-->
-            <div x-show="open">
-                <div :class="{'block':open, 'hidden': ! open}" class="hidden fixed inset-0 bg-white bg-opacity-75 flex items-center justify-center">
-                    <div class="flex flex-col bg-white shadow-2xl rounded-lg border-gray-400 p6">
-
-                        <div class="px-6 py-2">
-                            <h3 class="text-lg font-medium text-rose-800">
-                                @if($isEdit)
-                                    Editar Jogador
-                                @else
-                                    Criar Jogador
-                                @endif
-                            </h3>
-                            <form class="space-y-8 divide-y divide-gray-200">
-                                <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                    <div class="col-span-3">
-                                        <label for="name" class="text-sm font-medium text-gray-700">Name</label>
-                                        <input type="text" wire:model="name" id="name" autocomplete="name" class="w-full flex-1 flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 rounded-md sm:text-sm border-gray-300">
-                                        <div class="text-red-500 font-bold">
-                                            @error('name'){{$message}}@enderror
-                                        </div>
-                                    </div>
-                                    <div class="col-span-1">
-                                        <label for="age" class="text-sm font-medium text-gray-700">Idade</label>
-                                        <input type="number" wire:model="age" id="age" autocomplete="age" class="w-full flex-1 flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300">
-                                        <div class="text-red-500 font-bold">
-                                            @error('age'){{$message}}@enderror
-                                        </div>
-                                    </div>
-                                    <div class="col-span-2">
-                                        <label for="nationality" class="text-sm font-medium text-gray-700">Nacionalidade</label>
-                                        <input type="text" id="nationality" wire:model="nationality" rows="2" class="block w-full border border-gray-300 rounded-md">
-                                        <div class="text-red-500 font-bold">
-                                            @error('nationality'){{$message}}@enderror
-                                        </div>
-                                    </div>
-
-                                    <div class="col-span">
-                                        <label for="wins" class="text-sm font-medium text-gray-700">Vitorias</label>
-                                        <input type="number" id="wins" wire:model="wins" class="block w-full border border-gray-300 rounded-md">
-                                        <div class="text-red-500 font-bold">
-                                            @error('wins'){{$message}}@enderror
-                                        </div>
-                                    </div>
-
-                                    <div class="col-span">
-                                        <label for="defeats" class="text-sm font-medium text-gray-700">Derrotas</label>
-                                        <input type="number" id="defeats" wire:model="defeats" class="block w-full border border-gray-300 rounded-md">
-                                        <div class="text-red-500 font-bold">
-                                            @error('defeats'){{$message}}@enderror
-                                        </div>
-                                    </div>
-
-
-                                    <div class="col-span-2">
-                                        @if($isEdit)
-                                            <button type="submit" x-on:click="open = false" wire:click.prevent="updatePlayer" class="bg-blue-500 py-2 px-3 rounded-md shadow-sm text-sm hover:bg-purple-500 font-medium text-white">Editar Jogador</button>
-                                        @else
-                                            <button type="submit" x-on:click="open = false" wire:click.prevent="submit" class="bg-blue-500 py-2 px-3 rounded-md shadow-sm text-sm hover:bg-purple-500 font-medium text-white">Criar Jogador</button>
-                                        @endif
-
-                                        <button type="button" x-on:click="open = false" wire:click="clearForm" class="bg-red-500 py-2 px-3 rounded-md shadow-sm text-sm hover:bg-red-800 font-medium text-white cursor-pointer">Fechar</button>
-
-                                    </div>
-                                </div>
-                            </form>
+            <!--MODAL-->
+            @component('components.modal')
+                @slot('header')
+                    @if($isEdit)
+                        Editar {{$title ?? null}}
+                    @else
+                        Criar {{$title ?? null}}
+                    @endif
+                @endslot
+                @slot('content')
+                    <div class="col-span-3">
+                        <label for="name" class="text-sm font-medium text-gray-700">Name</label>
+                        <input type="text" wire:model="name" id="name" autocomplete="name" class="w-full flex-1 flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 rounded-md sm:text-sm border-gray-300">
+                        <div class="text-red-500 font-bold">
+                            @error('name'){{$message}}@enderror
                         </div>
                     </div>
-                </div>
-            </div>
+                    <div class="col-span-1">
+                        <label for="age" class="text-sm font-medium text-gray-700">Idade</label>
+                        <input type="number" wire:model="age" id="age" autocomplete="age" class="w-full flex-1 flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300">
+                        <div class="text-red-500 font-bold">
+                            @error('age'){{$message}}@enderror
+                        </div>
+                    </div>
+                    <div class="col-span-2">
+                        <label for="nationality" class="text-sm font-medium text-gray-700">Nacionalidade</label>
+                        <input type="text" id="nationality" wire:model="nationality" rows="2" class="block w-full border border-gray-300 rounded-md">
+                        <div class="text-red-500 font-bold">
+                            @error('nationality'){{$message}}@enderror
+                        </div>
+                    </div>
+
+                    <div class="col-span">
+                        <label for="wins" class="text-sm font-medium text-gray-700">Vitorias</label>
+                        <input type="number" id="wins" wire:model="wins" class="block w-full border border-gray-300 rounded-md">
+                        <div class="text-red-500 font-bold">
+                            @error('wins'){{$message}}@enderror
+                        </div>
+                    </div>
+
+                    <div class="col-span">
+                        <label for="defeats" class="text-sm font-medium text-gray-700">Derrotas</label>
+                        <input type="number" id="defeats" wire:model="defeats" class="block w-full border border-gray-300 rounded-md">
+                        <div class="text-red-500 font-bold">
+                            @error('defeats'){{$message}}@enderror
+                        </div>
+                    </div>
+                @endslot
+                @slot('option')
+                    @if($isEdit)
+                        <button type="submit" x-on:click="open = false" wire:click.prevent="updatePlayer" class="bg-blue-500 py-2 px-3 rounded-md shadow-sm text-sm hover:bg-purple-500 font-medium text-white">Editar Jogador</button>
+                    @else
+                        <button type="submit" x-on:click="open = false" wire:click.prevent="submit" class="bg-blue-500 py-2 px-3 rounded-md shadow-sm text-sm hover:bg-purple-500 font-medium text-white">Criar Jogador</button>
+                    @endif
+                @endslot
+            @endcomponent
 
             <!--TABLE-->
             <div class="w-full col-span-3 mt-8">
